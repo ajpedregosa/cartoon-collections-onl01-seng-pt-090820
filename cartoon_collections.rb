c993f7f3f81e111c5195560de35489c2b9e07ad3
Jay Pedregosa <alexanderj.pedregosa@gmail.com>
2020-09-21 20:54:05 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,5 +1,5 @@
 def roll_call_dwarves(array_dwarf_names)
-
+  
 end
 
 def summon_captain_planet# code an argument here
@@ -13,4 +13,17 @@ end
 def find_the_cheese# code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+end
+
+
+def line(katz_deli)
+  line_array = []
+  if katz_deli.length == 0
+    puts "The line is currently empty."
+  else
+    katz_deli.each.with_index(1) do |name, index|
+    line_array.push("#{index}. #{name}")
+    end
+    puts "The line is currently: #{line_array.join(" ")}"
+  end
 end
